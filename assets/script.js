ef3f4b73578de1f70d699f394fde14d121bfb2b8
Jas <ragnarock.josef@gmail.com>
2021-07-18 16:30:39 -0400

    fixed questions, added features $ styles

--- assets/script.js
+++ assets/script.js
@@ -1,59 +1,80 @@
+//Global variables for score, time, highscores
 let scoreCount = 0;
-let secondsLeft = 5;
+let secondsLeft = 10;
 let highScores = [];
+let scores = [];
 
+//Selectors for the start button and time display
 const timeEl = document.querySelector(".time");
 const start = document.querySelector(".start");
 
+//selectors for the questions
 const qSection = document.querySelector(".qSection");
 const questionOne = document.getElementById("question-one");
 const questionTwo = document.getElementById("question-two");
 const questionThree = document.getElementById("question-three");
 const questionFour = document.getElementById("question-four");
 
-
+//selectors for the question answers
 const answerEl = document.querySelector(".one-true");
 const answerElTwo = document.querySelector(".two-true");
 const answerElThree = document.querySelector(".three-true");
 const answerElFour = document.querySelector(".four-true");
 
-const wrong = document.querySelector(".false");
-
+//selectors for the score section and intials entry 
 const scoreSection = document.querySelector(".scoreSection");
-let listOfScores = document.querySelector("#highscore-list");
 let intialsInput = document.querySelector("#initials");
 const intialsConfrim = document.querySelector(".confirm-initials");
 
+//selectors for the score area
+let scoreList = document.querySelector("#highscore-list");
+let highScoreArea = document.querySelector(".highscores");
+
+//selectors for the description, gameover, and score button
 const Desc = document.getElementById("description");
 const gameover = document.getElementById("gameover");
+const scoreBtn = document.querySelector(".score-btn")
 
-const questions = [
-    questionOne,
-    questionTwo,
-    questionThree,
-    questionFour
-]
+//selector for the retry button 
+const retryBtn = document.querySelector(".retry");
 
-function init() {
-    getScore();
-    getInitials();
-    scoreList();
-}
+let list = document.getElementById("myList");
+
 
 function getScore() {
     localStorage.getItem("score", scoreCount);
 }
 
-function getInitials() {
-    localStorage.getItem("intials", JSON.stringify(highScores))
+//renders the scores upon page load
+function init() {
+    renderScores();
+    getScore();
 }
 
+function renderScores() {
+    let data = JSON.parse(localStorage.getItem("highScore"));
+
+    data.forEach((item) => {
+        let li = document.createElement("li");
+        li.innerText = JSON.stringify(item);
+        list.appendChild(li);
+    })
+
+    let storedHighScores = JSON.parse(localStorage.getItem("scores"));
+
+    if (storedHighScores !== null) {
+        highScores = storedHighScores;
+    }
+}
+
+//Calls the function to hide text, show questions, and start the timer
 function startQuiz() {
     hideText();
     setTime();
     questionsStart();
 }
 
+//this function removes the start and score buttons and and descriptions
 function hideText() {
     start.classList.remove("show");
     start.classList.add("hide");
@@ -62,11 +83,13 @@ function hideText() {
     Desc.classList.add("hide");
 }
 
+//This function shows the questions when called 
 function questionsStart() {
     questionOne.classList.remove("hide");
     questionOne.classList.add("show");
 }
 
+//This function creates a global timer that counts down from 10 and sends a function on o
 function setTime() {
     let timerInterval = setInterval(function () {
         secondsLeft--;
@@ -80,6 +103,8 @@ function setTime() {
     }, 1000);
 }
 
+//When called, the timer element is set to an empty string
+//The questions are removed, and the name entry and gameover screen is shown
 function sendMessage() {
     timeEl.textContent = "";
     qSection.classList.remove("show");
@@ -91,75 +116,112 @@ function sendMessage() {
     scoreSection.classList.add("show");
 }
 
-function scoreList() {
-    for (let i = 0; i < listOfScores.length; i++) {
-        let listChild = listOfScores[i];
-
-        let li = document.createElement("li");
-        li.textContent = listChild;
-        highScores.appendChild(listChild);
-        console.log(highScores);
-    }
-}
-
-
-intialsConfrim.addEventListener("click", function () {
-    let highScores = {
+function createHighScore() {
+    let newHighScores = {
         intials: intialsInput.value.trim().toUpperCase(),
         score: scoreCount
     };
+
+    highScores.push(JSON.stringify(newHighScores));
+
+    let scoreObj = JSON.stringify(highScores)
 
     if (intialsInput === "") {
         return;
     }
 
-    localStorage.setItem("highscore", JSON.stringify(highScores));
+    localStorage.setItem("highScore", scoreObj);
+}
+
+
+intialsConfrim.addEventListener("click", function () {
+
+    scoreSection.classList.add("hide");
+    scoreSection.classList.remove("show");
+
+    gameover.classList.add("hide");
+    gameover.classList.remove("show");
+
+    createHighScore();
+    renderScores();
+
+    highScoreArea.classList.add("show");
+    highScoreArea.classList.remove("hide");
+
+
 });
 
+//On click, startQuiz funtion is executed
 start.addEventListener("click", function (event) {
     event.preventDefault();
     startQuiz();
 });
 
+//This button hides everything and shows the highscores
+scoreBtn.addEventListener("click", function () {
+    highScoreArea.classList.add("show");
+    highScoreArea.classList.remove("hide");
+
+    timeEl.classList.add("hide");
+    timeEl.classList.remove("show");
+    hideText();
+
+
+});
+
+//These two event listeners attach a function to every true and false answer
+//If it is a true answer, the score will increase, if false, a second will be lost
+document.querySelectorAll(".true").forEach(item => {
+    item.addEventListener("click", function() {
+        let newScore = scoreCount++;
+        return newScore;
+    })
+});
+
+document.querySelectorAll(".false").forEach(item => {
+    item.addEventListener("click", function () {
+        secondsLeft--;
+        timeEl.textContent = secondsLeft + " second(s) remaining";
+    })
+});
+
+
+//These three event listeners hide the current question and show the next
 answerEl.addEventListener("click", function () {
-    let newScore = scoreCount++;
     questionOne.classList.remove("show");
     questionOne.classList.add("hide");
 
     questionTwo.classList.remove("hide");
     questionTwo.classList.add("show");
-    return newScore;
 });
 
 answerElTwo.addEventListener("click", function () {
-    let newScore = scoreCount++;
     questionTwo.classList.remove("show");
     questionTwo.classList.add("hide");
 
     questionThree.classList.remove("hide");
     questionThree.classList.add("show");
-    return newScore;
+
 });
 
 answerElThree.addEventListener("click", function () {
-    let newScore = scoreCount++;
     questionThree.classList.remove("show");
     questionThree.classList.add("hide");
 
     questionFour.classList.remove("hide");
     questionFour.classList.add("show");
-    return newScore;
+
 });
 
+//This fourth event listener will set the time = 0 triggering the gameover state
 answerElFour.addEventListener("click", function () {
-    let newScore = scoreCount++;
     secondsLeft = 0;
-    return newScore;
 });
 
-wrong.addEventListener("click", function () {
-    secondsLeft--;
-    timeEl.textContent = secondsLeft + " second(s) remaining";
+//This event listener reloads the page and allows the user to try again
+retryBtn.addEventListener("click", function () {
+    history.go(0);
 });
 
+//this initializes the application
 init();
